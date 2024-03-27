307a160dbfa2374427ce37193e29440fbc61e423
Benoit <fbenoit2077@gmail.com>
2024-03-27 17:08:00 -0400

    components organize

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
-import React from 'react';
-import Header from './components/header';
-import Footer from './components/footer';
-import Portfolio from './components/portfolio';
-import './App.css';
+import styles from "./App.module.css";
+import { About } from "./components/About/About";
+import { Contact } from "./components/Contact/Contact";
+import { Experience } from "./components/Experience/Experience";
+import { Hero } from "./components/Hero/Hero";
+import { Navbar } from "./components/Navbar/Navbar";
+import { Projects } from "./components/Projects/Projects";
 
 function App() {
   return (
-    <>
-      <Header />
-      <Portfolio />
-      <Footer />
-    </>
+    <div className={styles.App}>
+      <Navbar />
+      <Hero />
+      <About />
+      <Experience />
+      <Projects />
+      <Contact />
+    </div>
   );
 }
 
