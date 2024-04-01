743bb53b950535e45fac8712cd83c9c249508fda
Benoit <fbenoit2077@gmail.com>
2024-04-01 19:25:41 -0400

    aboutjsx

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -8,7 +8,7 @@ export const About = () => {
       <h2 className={styles.title}>About</h2>
       <div className={styles.content}>
         <img
-          src={getImageUrl("about/aboutImage.png")}
+          src={getImageUrl("about/aboutImage.gif")}
           alt="Me sitting with a laptop"
           className={styles.aboutImage}
         />
