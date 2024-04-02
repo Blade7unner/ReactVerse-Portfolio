c082b41f24fd63842bfa5b12854547a38656a1f1
Benoit <fbenoit2077@gmail.com>
2024-04-02 01:15:18 -0400

    last commit 1

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,8 @@
-import React, { useState } from "react";
-
+import React from "react";
 import styles from "./Navbar.module.css";
 import { getImageUrl } from "../../utils";
 
 export const Navbar = () => {
-  const [menuOpen, setMenuOpen] = useState(false);
-
   return (
     <nav className={styles.navbar}>
       <a className={styles.title} href="/">
@@ -14,18 +11,15 @@ export const Navbar = () => {
       <div className={styles.menu}>
         <img
           className={styles.menuBtn}
-          src={
-            menuOpen
-              ? getImageUrl("nav/closeIcon.png")
-              : getImageUrl("nav/menuIcon.png")
-          }
+          src={getImageUrl("nav/menuIcon.png")}
           alt="menu-button"
-          onClick={() => setMenuOpen(!menuOpen)}
         />
-        <ul
-          className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`}
-          onClick={() => setMenuOpen(false)}
-        >
+        <img
+          className={`${styles.closeIcon} ${styles.menuBtn}`}
+          src={getImageUrl("nav/closeIcon.png")}
+          alt="close-button"
+        />
+        <ul className={styles.menuItems}>
           <li>
             <a href="#about">About</a>
           </li>
