ef0e9418648e14069a9075f3cafe74180fd17d6f
Benoit <fbenoit2077@gmail.com>
2024-04-01 20:06:55 -0400

    styles fix

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -19,7 +19,7 @@ export const Hero = () => {
         </a>
       </div>
       <img
-        src={getImageUrl("hero/heroImage.png")}
+        src={getImageUrl("hero/heroImage.gif")}
         alt="image of me"
         className={styles.heroImg}
       />
